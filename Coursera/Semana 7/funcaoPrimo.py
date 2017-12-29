2371b23a9e3c1a4a12b15c79c2871b44e5c014a8
lc-leonesse <33597823+lc-leonesse@users.noreply.github.com>
2017-12-29 16:17:46 -0200

    29_12_V2

---
+++ Coursera/Semana 7/funcaoPrimo.py
@@ -0,0 +1,27 @@
+"""
+
+Informar se o número que o usuário está digitando é primo ou não
+
+"""
+
+
+def primo(n):
+    divisores = 0
+    i = 1
+    while i <= n:
+        if n % i == 0:
+            divisores = divisores + 1
+        i = i + 1
+    if divisores == 2:
+        return True
+    else:
+        return False
+
+n = int(input("Digite um número inteiro: "))
+
+while n > 0:
+    if primo(n) == True:
+        print("é primo!")
+    else:
+        print("não é primo!")
+    n = int(input("Digite um número inteiro: "))
