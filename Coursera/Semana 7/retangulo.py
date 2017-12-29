f943e873cab9b0da49644395ff5e3fdf8e19f2b8
lc-leonesse <33597823+lc-leonesse@users.noreply.github.com>
2017-12-29 16:46:14 -0200

    29_12_V2

---
+++ Coursera/Semana 7/retangulo.py
@@ -0,0 +1,28 @@
+"""
+Escreva um programa que recebe como entradas (utilize a função input)
+dois números inteiros correspondentes à largura e à altura de um
+retângulo, respectivamente. O programa deve imprimir uma cadeia de
+caracteres que represente o retângulo informado com caracteres '#'
+na saída.
+
+Por exemplo:
+
+digite a largura: 10
+digite a altura: 3
+
+##########
+##########
+##########
+
+digite a largura: 2
+digite a altura: 2
+##
+##
+
+"""
+
+
+largura: int(input("Digite a largura: "))
+altura : int(input("Digite a altura"))
+
+print()
