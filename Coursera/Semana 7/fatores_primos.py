da63abd6a5ddfc8c5a57766efb00c22b6a32e923
lc-leonesse <33597823+lc-leonesse@users.noreply.github.com>
2017-12-29 16:06:34 -0200

    29_12_V2

--- Coursera/Semana 7/fatores_primos.py
+++ Coursera/Semana 7/fatores_primos.py
@@ -8,3 +8,17 @@ cada fator.
 1000 = 2 ^ 3 * 5 ^ 3
 
 """
+
+n = int(input("Digite um número inteiro > 1: "))
+
+fator = 2
+multiplicidade = 0
+
+while n > 1:
+    while n % fator == 0:
+        multiplicidade = multiplicidade + 1
+        n = n / fator
+    if multiplicidade > 0:
+        print("fator ", fator, "multiplicidade = ", multiplicidade)
+    fator = fator + 1
+    multiplicidade = 0
