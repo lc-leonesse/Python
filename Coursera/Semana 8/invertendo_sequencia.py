668ca151e8db0eac6e5bb2c15304f39d64b06e1d
Luiz <33597823+lc-leonesse@users.noreply.github.com>
2018-01-13 20:37:18 -0200

    13-01-2018

--- Coursera/Semana 8/invertendo_sequencia.py
+++ Coursera/Semana 8/invertendo_sequencia.py
@@ -1,7 +1,7 @@
 """
 
-Lê uma sequeência de números do teclado. Quanso o usuário digitar 0, imprimir a lista em ordem inversa a digitada
-pelo usuário
+Como pedido na primeira video-aula desta semana, escreva um programa que recebe uma sequência de números inteiros
+terminados por 0 e imprima todos os valores em ordem inversa. Note que 0 (ZERO) não deve fazer parte da sequência.
 
 lista[start:end:step] - slice
 
@@ -20,11 +20,11 @@ a[:-2]   # everything except the last two items
 lista = []
 
 
-numero = int(input("Informe um número. Para terminar, informe 0 (zero): "))
+numero = int(input("Digite um número: "))
 
 
 while numero != 0:
     lista.append(numero)
-    numero = int(input("Informe um número. Para terminar, informe 0 (zero): "))
+    numero = int(input("Digite um número: "))
     if numero == 0:
         print(lista[::-1])  # lista do final para o início. Step = -1
