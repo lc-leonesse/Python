5c0887b628e8f6525860f8dbc75d9b300efd7675
Luiz <33597823+lc-leonesse@users.noreply.github.com>
2018-01-13 18:01:38 -0200

    13-01-2018

---
+++ Coursera/Semana 8/elementos_repetidos.py
@@ -0,0 +1,12 @@
+"""
+
+Escreva a função remove_repetidos que recebe como parâmetro uma lista com números inteiros, verifica se tal
+lista possui elementos repetidos e os remove. A função deve devolver uma lista correspondente à primeira lista,
+sem elementos repetidos. A lista devolvida deve estar ordenada.
+
+Dica: Você pode usar lista.sort() ou sorted(lista). Qual a diferença?
+
+"""
+
+def remove_repetidos(lista):
+    for x in lista:
--- Coursera/Semana 8/listas.py
+++ Coursera/Semana 8/listas.py
@@ -3,16 +3,28 @@
 Lê uma sequeência de números do teclado. Quanso o usuário digitar 0, imprimir a lista em ordem inversa a digitada
 pelo usuário
 
+lista[start:end:step] - slice
+
+a[start:end] # items start through end-1
+a[start:]    # items start through the rest of the array
+a[:end]      # items from the beginning through end-1
+a[:]         # a copy of the whole array
+
+a[-1]    # last item in the array
+a[-2:]   # last two items in the array
+a[:-2]   # everything except the last two items
 
 """
 
 
 lista = []
 
+
 numero = int(input("Informe um número. Para terminar, informe 0 (zero): "))
+
 
 while numero != 0:
     lista.append(numero)
+    numero = int(input("Informe um número. Para terminar, informe 0 (zero): "))
     if numero == 0:
-        comprimento = len(lista)
-        print(lista[])
+        print(lista[::-1])  # lista do final para o início. Step = -1
