24597b8e04f9b45c5f6fb14a1ad7945819bc46c8
lc-leonesse <33597823+lc-leonesse@users.noreply.github.com>
2018-01-25 10:37:46 -0200

    25-01-2018

--- Coursera - Parte 2/Semana 3/triangulos_semelhantes.py
+++ Coursera - Parte 2/Semana 3/triangulos_semelhantes.py
@@ -13,8 +13,6 @@ diferente e ordená-las.
 """
 
 
-
-
 class Triangulo:
     def __init__(self, a, b, c):
         self.lados = sorted([a, b, c])
@@ -22,12 +20,16 @@ class Triangulo:
     def semelhantes(self,triangulo):
         x = triangulo.lados
         if x[0] >= self.lados[0] and x[1] >= self.lados[1] and x[2] >= self.lados[2]:
-            if x[0] % self.lados[0] == 0 and x[1] % self.lados[1] == 0 and x[2] % self.lados[2] == 0:
+            if x[0] - self.lados[0] == x[1] - self.lados[1] == x[2] - self.lados[2]:
+                return True
+            elif x[0] % self.lados[0] == 0 and x[1] % self.lados[1] == 0 and x[2] % self.lados[2] == 0:
                 return True
             else:
                 return False
         if x[0] <= self.lados[0] and x[1] <= self.lados[1] and x[2] <= self.lados[2]:
-            if self.lados[0] % x[0] == 0 and self.lados[1] % x[1] == 0 and self.lados[2] % x[2] == 0:
+            if self.lados[0] - x[0] == 0 and self.lados[1] - x[1] == 0 and self.lados[2] - x[2] == 0:
+                return True
+            elif self.lados[0] % x[0] == 0 and self.lados[1] % x[1] == 0 and self.lados[2] % x[2] == 0:
                 return True
             else:
                 return False
