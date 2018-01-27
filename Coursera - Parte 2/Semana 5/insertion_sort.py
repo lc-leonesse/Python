d8d812c219626383ac686688a029ab87f5c30239
lc-leonesse <33597823+lc-leonesse@users.noreply.github.com>
2018-01-27 14:35:13 -0200

    27-01-2018

--- Coursera - Parte 2/Semana 5/insertion_sort.py
+++ Coursera - Parte 2/Semana 5/insertion_sort.py
@@ -13,16 +13,14 @@ def insertion_sort(lista):
 
         while posicao > 0 and lista[posicao-1] > valor_atual:
             lista[posicao] = lista[posicao-1]
+            posicao = posicao - 1
 
         lista[posicao] = valor_atual
 
-    return lista
 
 
 
 
 
 
-lista = [14,46,43,27,57,41,45,21,70]
 
-print(insertion_sort(lista))
